d2745239c445aaf25c9ccbf03c28e25bf4048b70
Jose Fallas <jofallas@microsoft.com>
2025-06-12 02:01:28 -0600

    Rename variable and clean up comments

--- problem1.py
+++ problem1.py
@@ -1,17 +1,13 @@
 #Problem 1: Multiples of 3 or 5
 
-#create empty array and variable for sum of multiples
-my_array = []
+#create empty array for multiples and variable for sum of multiples
+multiples = []
 multiples_sum = 0
 
 #find multiples of 3 or 5 below 1000
 for i in range(1,1000):
     if(i%3==0 or i%5==0):
        my_array.append(i) 
-
-#print values in array
-#for i in range(0,len(my_array)):
-#    print(my_array[i])
 
 #perform sum of the multiples
 for i in range (0,len(my_array)):
