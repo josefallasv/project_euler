d027e568d4e4c868bdd200c597ca4c35741cdca8
Jose Fallas <jofallas@microsoft.com>
2025-06-12 02:05:55 -0600

    Changed and added a few comments

--- problem1.py
+++ problem1.py
@@ -1,17 +1,18 @@
 #Problem 1: Multiples of 3 or 5
+#Link to problem: https://projecteuler.net/problem=1
 
-#create empty array for multiples and variable for sum of multiples
+#Create empty array for multiples and variable for sum of multiples
 multiples = []
 multiples_sum = 0
 
-#find multiples of 3 or 5 below 1000
+#Find multiples of 3 or 5 below 1000
 for i in range(1,1000):
     if(i%3==0 or i%5==0):
        my_array.append(i) 
 
-#perform sum of the multiples
+#Perform sum of the multiples
 for i in range (0,len(my_array)):
     multiples_sum+=my_array[i]
 
-#print sum of multiples
+#Print sum of multiples
 print("the sum of multiples of 3 or 5 below 1000 is", multiples_sum)
