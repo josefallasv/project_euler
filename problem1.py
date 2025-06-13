8e301a5764fdf3a9e467a9b6ad8a2ee0d4c97436
Jose Fallas <jofallas@microsoft.com>
2025-06-12 18:37:31 -0600

    Updated code to fix references of renamed array

--- problem1.py
+++ problem1.py
@@ -8,11 +8,11 @@ multiples_sum = 0
 #Find multiples of 3 or 5 below 1000
 for i in range(1,1000):
     if(i%3==0 or i%5==0):
-       my_array.append(i) 
+       multiples.append(i) 
 
 #Perform sum of the multiples
-for i in range (0,len(my_array)):
-    multiples_sum+=my_array[i]
+for i in range (0,len(multiples)):
+    multiples_sum+=multiples[i]
 
 #Print sum of multiples
 print("the sum of multiples of 3 or 5 below 1000 is", multiples_sum)
